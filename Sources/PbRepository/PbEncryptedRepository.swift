3109bad6237b21aacba160f5e1e96de24a0ab3ab
Piotr Boguslawski <boguslawski.piotr@gmail.com>
2022-01-20 21:36:06 +0100

    Started work on PbEncryptedRepository.

---
+++ Sources/PbRepository/PbEncryptedRepository.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+/// Decorator for classes that conforms to the PbRepository / PbRepositoryAsync protocols
+/// providing encryption of stored files and of course decrypting when retrieving.
+public class PbEncryptedRepository
+{
+    
+}
